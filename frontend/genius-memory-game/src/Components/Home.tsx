58a0409a3fb4e5b395d8d49d3cd3c3cba5174728
Eduardo Santos <edupaz077@gmail.com>
2024-12-23 23:34:34 -0300

    Lógica do jogo feita

--- frontend/genius-memory-game/src/Components/Home.tsx
+++ frontend/genius-memory-game/src/Components/Home.tsx
@@ -1,14 +1,39 @@
+import { useEffect } from 'react';
 import '../Styles/Home.css'
 import ButtonLink from './ButtonLink'
+import introJs from 'intro.js';
+import 'intro.js/introjs.css';
 
 function Home() {
- 
+  useEffect(() => {
+    introJs().setOptions({
+      steps: [
+        {
+          intro:  `<div style="text-align: center;">
+          Bem-vindo ao Genius Game 
+          <img src="/maka.gif" alt="Genius Game" style="width: 200px; height: auto;  margin-top: 20px;" />
+        </div>`
+        },
+        {
+          element: '.GameModes',
+          intro: 'Escolha seu modo de jogo aqui',
+        },
+        {
+          element: '.Link',
+          intro: 'Se você for novo tente esse!',
+        },
+        
+      ],
+    }).start();
+  }, []); 
     return (
       <>
+      
         <h1>Genius Game</h1>
         <div className='GameModes'>
         <ButtonLink buttontext='Solo Game' to='/sologame'></ButtonLink>
-        <ButtonLink buttontext="1v1" to='/sologame'></ButtonLink>
+        <ButtonLink buttontext="1v1" to='/'></ButtonLink>
+        <ButtonLink buttontext="Co-op" to='/'></ButtonLink>
         </div>
         
       </>
--- frontend/genius-memory-game/src/Components/SoloGame.tsx
+++ frontend/genius-memory-game/src/Components/SoloGame.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import '../Styles/SoloGame.css'
 import incorrectButton from '../Sounds/error-8-206492.mp3'
 import correctButton from '../Sounds/new-notification-7-210334.mp3'
+
 function SoloGame() {
 
   const coresDisponiveis = ['Vermelho', 'Amarelo', 'Verde', 'Azul'];
@@ -21,14 +22,42 @@ function SoloGame() {
     console.log(numeroAleatorio);
     setCores(cores.concat(corSelecionada));
     setUltimaCor(corSelecionada)
+    
   }, [rodada])
-
   console.log(cores);
+  
+  useEffect(() => {
+    /* const botoes = document.querySelectorAll<HTMLButtonElement>(`.Vermelho, .Amarelo, .Verde, .Azul`);
 
+  for (let i = 0; i < botoes.length; i++) {
+    botoes[i].disabled = true;
+  } */
+
+    for (let i = 0; i < cores.length; i++) {
+      const piscarCores = document.querySelector<HTMLButtonElement>(`.${cores[i]}`)!;
+      
+      setTimeout(() => {
+        piscarCores.style.backgroundColor = 'rgb(240, 240, 240)';
+      }, i* 800);
+  
+      setTimeout(() => {
+          piscarCores.style.backgroundColor = '';
+      }, i * 800 + 600); 
+    }
+    
+  }, [cores])
+  
   function Sequencia(corEscolhidaPeloPlayer: string) {
     setEscolhasDoPlayer(escolhasDoPlayer.concat(corEscolhidaPeloPlayer));
     const acerto = corEscolhidaPeloPlayer === cores[escolhasDoPlayer.length];
-    console.log(escolhasDoPlayer);
+    // ex: a sequencia é [ "Vermelho", "Azul", "Azul"]
+    // cores[0] é "Vermelho"
+    // cores[1] é "Azul"
+    // cores[2] é "Azul" 
+
+    // se o jogador já escolheu a primeira cor a segunda seria:
+    // corEscolhidaPeloPlayer === cores[1] que seria "Azul"
+    
 
     if (acerto) {
       TocarAudio(correctButton)
@@ -39,7 +68,7 @@ function SoloGame() {
       }, 220);
       
       if (escolhasDoPlayer.length + 1 === cores.length) {
-        
+        //aqui precisa do + 1 já que estado escolhasDoPlayer ainda não foi atualizado (não me pergunte o porque)
         setRodada(rodada + 1);
         setEscolhasDoPlayer([]);
       }
@@ -62,19 +91,20 @@ function SoloGame() {
       }
     }
   }
-
+  console.log(escolhasDoPlayer.length);
+  console.log(cores.length);
   return (
     <>
       <h1>Rodada {rodada}</h1>
-      <h2>Sequencia das cores: {cores.join(' ')}</h2>
+     {/*  <h2>Sequencia das cores: {cores.join(' ')}</h2> */}
       <h2>Cor da rodada: {ultimaCor}</h2>
       <div className="Buttons">
-        <button className='Red' onClick={() => { Sequencia('Vermelho') }}>Vermelho</button>
-        <button className='Yellow' onClick={() => { Sequencia('Amarelo') }}>Amarelo</button>
+        <button className='Vermelho' onClick={() => { Sequencia('Vermelho') }}>Vermelho</button>
+        <button className='Amarelo' onClick={() => { Sequencia('Amarelo') }}>Amarelo</button>
       </div>
       <div className="Buttons">
-        <button className='Green' onClick={() => { Sequencia('Verde') }}>Verde</button>
-        <button className='Blue' onClick={() => { Sequencia('Azul') }}>Azul</button>
+        <button className='Verde' onClick={() => { Sequencia('Verde') }}>Verde</button>
+        <button className='Azul' onClick={() => { Sequencia('Azul') }}>Azul</button>
       </div>
 
     </>
